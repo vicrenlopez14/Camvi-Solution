a93bef1fe2066c4a18ab1560c4886da2bc3eaac4
Adriana Orellana <104051186+orellanaoa@users.noreply.github.com>
2023-05-03 23:12:38 -0600

    Pantalla detalles terminada

--- app/src/main/java/com/example/camvi/ui/screens/global/DetalleMisCitas.kt
+++ app/src/main/java/com/example/camvi/ui/screens/global/DetalleMisCitas.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.camvi.R
 import com.example.camvi.ui.widgets.global.CamviScreen
+import com.example.camvi.model.administradores.Sesiones
 
 @Composable
 @Preview
@@ -50,11 +51,213 @@ fun DetalleMisCitas(){
                   )
             }
          Column(
-             modifier = Modifier.fillMaxSize()
+             modifier = Modifier
+                 .fillMaxSize()
+                 .padding(horizontal = 20.dp, vertical = 30.dp)
          ) {
+             Spacer(modifier = Modifier.height(15.dp))
+             Text(
+                text = "Sesión fotografica para cumpleaños", //aca va el titulo o sea variable
+                fontFamily = FontFamily(Font(R.font.inter_semibold)),
+                fontSize= 18.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(bottom = 5.dp)
+                    .padding(end = 25.dp)
+            )
+             Spacer(modifier = Modifier.height(30.dp))
+             InformacionEvento()
+             Spacer(modifier = Modifier.height(60.dp))
+             InformacionCliente()
 
          }
      }
+}
+
+@Composable
+fun InformacionEvento(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Evento",
+            fontFamily = FontFamily(Font(R.font.inter_semibold)),
+            fontSize= 16.sp,
+            color = Color.Black,
+            textAlign= TextAlign.Start,
+            modifier = Modifier.padding(end = 280.dp),
+            )
+             Spacer(modifier = Modifier.height(20.dp))
+             Row {
+                  Image(painter = painterResource(id = R.drawable.casa),
+                      contentDescription = null,
+                  modifier = Modifier
+                      .size(25.dp)
+                  )
+                 Spacer(modifier = Modifier.width(15.dp))
+                 Text(
+                     text = "Lugar",
+                     fontFamily= FontFamily(Font(R.font.inter)),
+                     fontSize = 13.sp,
+                     color = Color.Gray
+                 )
+                 Spacer(modifier = Modifier.width(30.dp))
+                 Text(text = "Lugar", //aca va de nuevo la variable
+                    fontFamily= FontFamily(Font(R.font.inter)),
+                     fontWeight = FontWeight.Bold,
+                     fontSize = 13.sp,
+                 )
+             }
+             Spacer(modifier = Modifier.height(20.dp))
+             Row {
+                  Image(painter = painterResource(id = R.drawable.calendar),
+                      contentDescription = null,
+                  modifier = Modifier
+                      .size(25.dp)
+                  )
+                 Spacer(modifier = Modifier.width(15.dp))
+                 Text(
+                     text = "Fecha",
+                     fontFamily= FontFamily(Font(R.font.inter)),
+                     fontSize = 13.sp,
+                     color = Color.Gray
+                 )
+                 Spacer(modifier = Modifier.width(30.dp))
+                 Text(text = "29/05/2023", //aca va de nuevo la variable
+                    fontFamily= FontFamily(Font(R.font.inter)),
+                     fontWeight = FontWeight.Bold,
+                     fontSize = 13.sp,
+                 )
+             }
+             Spacer(modifier = Modifier.height(20.dp))
+             Row {
+                  Image(painter = painterResource(id = R.drawable.reloj),
+                      contentDescription = null,
+                  modifier = Modifier
+                      .size(25.dp)
+                  )
+                 Spacer(modifier = Modifier.width(15.dp))
+                 Text(
+                     text = "Hora inicio",
+                     fontFamily= FontFamily(Font(R.font.inter)),
+                     fontSize = 13.sp,
+                     color = Color.Gray
+                 )
+                 Spacer(modifier = Modifier.width(30.dp))
+                 Text(text = "3: 00 pm", //aca va de nuevo la variable
+                    fontFamily= FontFamily(Font(R.font.inter)),
+                     fontWeight = FontWeight.Bold,
+                     fontSize = 13.sp,
+                 )
+             }
+             Spacer(modifier = Modifier.height(20.dp))
+             Row {
+                  Image(painter = painterResource(id = R.drawable.reloj),
+                      contentDescription = null,
+                  modifier = Modifier
+                      .size(25.dp)
+                  )
+                 Spacer(modifier = Modifier.width(15.dp))
+                 Text(
+                     text = "Hora finalización",
+                     fontFamily= FontFamily(Font(R.font.inter)),
+                     fontSize = 13.sp,
+                     color = Color.Gray
+                 )
+                 Spacer(modifier = Modifier.width(30.dp))
+                 Text(text = "6: 00 pm", //aca va de nuevo la variable
+                    fontFamily= FontFamily(Font(R.font.inter)),
+                     fontWeight = FontWeight.Bold,
+                     fontSize = 13.sp,
+                 )
+             }
+
+
+    }
+
+}
+
+@Composable
+fun InformacionCliente(){
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Cliente",
+            fontFamily = FontFamily(Font(R.font.inter_semibold)),
+            fontSize= 16.sp,
+            color = Color.Black,
+            textAlign= TextAlign.Start,
+            modifier = Modifier.padding(end = 280.dp),
+            )
+             Spacer(modifier = Modifier.height(20.dp))
+             Row {
+                  Image(painter = painterResource(id = R.drawable.personicon),
+                      contentDescription = null,
+                  modifier = Modifier
+                      .size(25.dp)
+                  )
+                 Spacer(modifier = Modifier.width(15.dp))
+                 Text(
+                     text = "Nombre",
+                     fontFamily= FontFamily(Font(R.font.inter)),
+                     fontSize = 13.sp,
+                     color = Color.Gray
+                 )
+                 Spacer(modifier = Modifier.width(30.dp))
+                 Text(text = "Instituto Tecnico Ricaldone", //aca va de nuevo la variable
+                    fontFamily= FontFamily(Font(R.font.inter)),
+                     fontWeight = FontWeight.Bold,
+                     fontSize = 13.sp,
+                 )
+             }
+             Spacer(modifier = Modifier.height(20.dp))
+             Row {
+                  Image(painter = painterResource(id = R.drawable.celular),
+                      contentDescription = null,
+                  modifier = Modifier
+                      .size(25.dp)
+                  )
+                 Spacer(modifier = Modifier.width(15.dp))
+                 Text(
+                     text = "Contacto",
+                     fontFamily= FontFamily(Font(R.font.inter)),
+                     fontSize = 13.sp,
+                     color = Color.Gray
+                 )
+                 Spacer(modifier = Modifier.width(30.dp))
+                 Text(text = "29/05/2023", //aca va de nuevo la variable
+                    fontFamily= FontFamily(Font(R.font.inter)),
+                     fontWeight = FontWeight.Bold,
+                     fontSize = 13.sp,
+                 )
+             }
+             Spacer(modifier = Modifier.height(20.dp))
+             Row {
+                  Image(painter = painterResource(id = R.drawable.dui),
+                      contentDescription = null,
+                  modifier = Modifier
+                      .size(25.dp)
+                  )
+                 Spacer(modifier = Modifier.width(15.dp))
+                 Text(
+                     text = "DUI",
+                     fontFamily= FontFamily(Font(R.font.inter)),
+                     fontSize = 13.sp,
+                     color = Color.Gray
+                 )
+                 Spacer(modifier = Modifier.width(30.dp))
+                 Text(text = "3: 00 pm", //aca va de nuevo la variable
+                    fontFamily= FontFamily(Font(R.font.inter)),
+                     fontWeight = FontWeight.Bold,
+                     fontSize = 13.sp,
+                 )
+             }
+    }
+
 
 }
 
--- app/src/main/java/com/example/camvi/ui/screens/global/GaleriaDeFotosScreen.kt
+++ app/src/main/java/com/example/camvi/ui/screens/global/GaleriaDeFotosScreen.kt
@@ -23,11 +23,13 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.camvi.R
 
 @Composable
+
 fun GalleryScreen() {
     Column(
         modifier = Modifier
