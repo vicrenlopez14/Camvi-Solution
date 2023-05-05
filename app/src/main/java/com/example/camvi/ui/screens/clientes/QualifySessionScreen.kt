f4ee3a44b3f74b7155fc06a1e99fef07fa3333ce
Adriana Orellana <104051186+orellanaoa@users.noreply.github.com>
2023-05-05 00:05:23 -0600

    Cambios pequeños

---
+++ app/src/main/java/com/example/camvi/ui/screens/clientes/QualifySessionScreen.kt
@@ -0,0 +1,11 @@
+package com.example.camvi.ui.screens.clientes
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+
+fun calificarSesion(){
+
+}
