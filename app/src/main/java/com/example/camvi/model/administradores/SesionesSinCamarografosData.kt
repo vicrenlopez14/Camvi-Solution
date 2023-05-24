0b7d7fdd30dbed2399cd76e52509bfce6b3cb840
Susan Castillo <susancasoli@gmail.com>
2023-05-23 20:49:11 -0600

    Lista sesiones sin camarografos asignados, falta proga de botones

---
+++ app/src/main/java/com/example/camvi/model/administradores/SesionesSinCamarografosData.kt
@@ -0,0 +1,18 @@
+package com.example.camvi.model.administradores
+
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class SesionesSinCamarografosData(
+
+    //val idSesion: Int,
+    val titulo: String,
+    val direccionEvento: String,
+    val fechaEvento: String,
+    val horaInicio: String,
+    val horaFinalizacion: String,
+    val lugar: String,
+    val confirmada: String,
+   //val idFotografo: Int,
+    //val idCliente: Int
+)
